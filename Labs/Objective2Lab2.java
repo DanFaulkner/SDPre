e7b4a5e0e14212a12b9d48a4ac9731091922adc6
Daniel Faulkner <danfaulkner0@gmail.com>
2021-08-30 19:28:00 -0600

    completed Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String []args) {
+    String name = "Daniel Faulkner";
+    int age = 27;
+    String state = "Alabama";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
