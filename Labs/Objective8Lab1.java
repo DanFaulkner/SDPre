7ce4deefc430fd566d76a63701878db86bf934ed
Daniel Faulkner <danfaulkner0@gmail.com>
2021-09-04 15:26:25 -0600

    completed Objective8Lab1.java

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for(int count = 1; count <= 10; count += 1) {
+      System.out.println(count);
+    }
+    for(int count = 10; count >= 1; count -= 1) {
+      System.out.println(count);
+    }
+  }
+}
